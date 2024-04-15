e130db5cadb2fb971e4590ab02fc20e603d925d4
Kiran <kiranchauhan162k@gmail.com>
2024-04-15 13:17:11 +0530

    Socket url updated

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/chat-app");
+        await mongoose.connect(process.env.MONGO_DB_URI);
+        // await mongoose.connect("mongodb://127.0.0.1:27017/chat-app");
         console.log("Connected to MongoDB");
 
     } catch (error) {
