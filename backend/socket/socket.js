dca6bc549d3d2e702babc9112f131080c7c31220
Kiran <kiranchauhan162k@gmail.com>
2024-04-15 13:55:21 +0530

    Socket url updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["https://chatapp-eq7e.onrender.com"],
+        // origin: ["http://localhost:3000"],
         methods: ["GET", "POST"],
     },
 });
