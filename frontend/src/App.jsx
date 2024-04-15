ae72585afe7f823337ad1066b189e2fdca2042a5
Kiran <kiranchauhan162k@gmail.com>
2024-04-15 16:44:02 +0530

    Socket url updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,13 @@ import SignUp from './pages/signup/SignUp.jsx';
 import { Toaster } from 'react-hot-toast';
 import { useAuthContext } from './context/AuthContext';
 
+import "assets/plugins/nucleo/css/nucleo.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/scss/argon-dashboard-react.scss";
+
+import AdminLayout from "./dashboard/src/layouts/Admin.js";
+import AuthLayout from "./dashboard/src/layouts/Auth.js";
+
 function App() {
   const {authUser} = useAuthContext();
   return (
@@ -14,6 +21,10 @@ function App() {
         <Route path='/' element={ authUser ? <Home /> : <Navigate to={'/login'} />  } />
         <Route path='/login' element={ authUser ? <Navigate to={'/'} /> : <Login /> } />
         <Route path='/signup' element={ authUser ? <Navigate to={'/'} /> : <SignUp /> } />
+
+        <Route path="/admin/*" element={<AdminLayout />} />
+        <Route path="/auth/*" element={<AuthLayout />} />
+        <Route path="*" element={<Navigate to="/admin/index" replace />} />
       </Routes>
       <Toaster />
     </div>
